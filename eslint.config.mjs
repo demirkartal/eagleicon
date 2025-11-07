0d29a6a16c6bccd7c0d53a5b495badf82f3e2acb
Cem Demirkartal <cemdemirkartal@gmail.com>
2025-11-07 10:10:35 +0300

    Create eslint.config.mjs

---
+++ eslint.config.mjs
@@ -0,0 +1,42 @@
+import eslint from '@eslint/js';
+import globals from 'globals';
+import stylistic from '@stylistic/eslint-plugin';
+import tseslint from 'typescript-eslint';
+
+export default [
+  eslint.configs.recommended,
+  stylistic.configs['recommended'],
+  {
+    ignores: ['node_modules/*', 'docs/*', '**/*.min.js', '**/*.min.mjs'],
+  },
+  {
+    languageOptions: {
+      ecmaVersion: 2016,
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+      },
+    },
+    rules: {
+      '@stylistic/semi': ['error', 'always'],
+      '@stylistic/max-len': ['warn', { ignoreUrls: true }],
+    },
+  },
+
+  // TypeScript
+  // https://github.com/typescript-eslint/typescript-eslint/tree/main/packages/eslint-plugin/src/configs
+  tseslint.configs.base,
+  tseslint.configs.eslintRecommended,
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.mts', '**/*.cts'],
+    languageOptions: {
+      parserOptions: {
+        project: './tsconfig.base.json',
+      },
+    },
+    rules: {
+      ...tseslint.configs.strictTypeChecked[2].rules,
+      ...tseslint.configs.stylisticTypeChecked[2].rules,
+    },
+  },
+];
