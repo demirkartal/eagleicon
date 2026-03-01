d0905d3ab0c22b50089b9886b25d9c74e24a3790
Cem Demirkartal <cemdemirkartal@gmail.com>
2026-03-01 14:21:01 +0300

    Update dist

--- dist/js/eagleicon.d.mts
+++ dist/js/eagleicon.d.mts
@@ -1,12 +1,13 @@
 /*!
- * EagleICON v0.0.5
- * Copyright 2022-2025 Cem Demirkartal
+ * EagleICON v0.0.6
+ * Copyright 2022-2026 Cem Demirkartal
  * Licensed under the MIT License
  */
 export default class EagleIcon {
+  protected doc: Document;
   protected prefix: string;
   protected spriteUrl: string;
-  constructor(spriteUrl: string, prefix?: string);
+  constructor(doc: Document, spriteUrl: string, prefix?: string);
   svgElement(id: string, extraClasses?: string[], extraAttributes?: Record<string, string>): SVGElement;
 }
 // # sourceMappingURL=eagleicon.d.mts.map
--- dist/js/eagleicon.mjs
+++ dist/js/eagleicon.mjs
@@ -1,22 +1,23 @@
 /*!
- * EagleICON v0.0.5
- * Copyright 2022-2025 Cem Demirkartal
+ * EagleICON v0.0.6
+ * Copyright 2022-2026 Cem Demirkartal
  * Licensed under the MIT License
  */
 export default class EagleIcon {
-  constructor(spriteUrl, prefix = 'eagleicon') {
+  constructor(doc, spriteUrl, prefix = 'eagleicon') {
     if (typeof spriteUrl !== 'string' || !spriteUrl.trim()) {
       throw new TypeError('EagleIcon: spriteUrl must be a non-empty string.');
     }
     this.spriteUrl = spriteUrl;
     this.prefix = prefix;
+    this.doc = doc;
   }
 
   svgElement(id, extraClasses = [], extraAttributes = {}) {
     if (typeof id !== 'string' || !id.trim()) {
       throw new TypeError('EagleIcon.svgElement: id must be a non-empty string.');
     }
-    const svgElem = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    const svgElem = this.doc.createElementNS('http://www.w3.org/2000/svg', 'svg');
     svgElem.classList.add(this.prefix, ...extraClasses);
     for (const key of Object.keys(extraAttributes)) {
       const value = extraAttributes[key];
@@ -24,7 +25,7 @@ export default class EagleIcon {
         svgElem.setAttribute(key, value);
       }
     }
-    const useElem = document.createElementNS('http://www.w3.org/2000/svg', 'use');
+    const useElem = this.doc.createElementNS('http://www.w3.org/2000/svg', 'use');
     useElem.setAttribute('href', `${this.spriteUrl}#${id}`);
     svgElem.append(useElem);
     return svgElem;
